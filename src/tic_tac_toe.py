8d9e4d952417c82a5c4d6f45715c29f7de4cfc19
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-10 23:53:51 -0500

    Write make_move function

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -4,3 +4,9 @@ def board_factory():
         (None, None, None),
         (None, None, None),
     )
+
+
+def make_move(board, piece, position):
+    mutable_board = [list(row) for row in board]
+    mutable_board[position[0]][position[1]] = piece
+    return tuple(tuple(row) for row in mutable_board)
