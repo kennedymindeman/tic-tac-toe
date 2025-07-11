0c765d636bca0ee0a4660f084969a4993e7c052c
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-10 23:46:46 -0500

    Add test for making a move

--- test/test_tic_tac_toe.py
+++ test/test_tic_tac_toe.py
@@ -8,3 +8,12 @@ def test_initial_state() -> None:
         (None, None, None),
     )
     assert board_factory() == board
+
+
+def test_make_move() -> None:
+    board = (
+        (None, None, None),
+        (None, None, None),
+        (None, None, None),
+    )
+    make_move(board, "x", (0, 0))
